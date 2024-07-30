b887c332e6b880b671500fb7d26db4a561698159
VITÓRIO AUGUSTO DA SILVA DE SOUZA <vitorio_souza@estudante.sesisenai.org.br>
2024-07-30 17:12:02 -0300

    Criação do projeto base em React com NodeJS

---
+++ projetoBase/introReact/src/components/Conteudo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Conteudo.css'
+
+function Conteudo() {
+  return (
+    <div className='conteudo-container'>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iusto fuga alias expedita repudiandae ipsam maxime suscipit impedit culpa tempora vero, iure laboriosam consequuntur iste numquam fugiat? Laborum quam dicta voluptatum!
+      </p>
+    </div>
+  )
+}
+
+export default Conteudo

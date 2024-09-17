d4736a23800826b4a0383bfd51b03e01a4e39593
VITÓRIO AUGUSTO DA SILVA DE SOUZA <vitorio_souza@estudante.sesisenai.org.br>
2024-09-17 16:49:42 -0300

    Atualização router

--- projetoRouter/src/contexts/GlobalConext.jsx
+++ projetoRouter/src/contexts/GlobalConext.jsx
@@ -1,13 +1,30 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const GlobalConext = createContext()
 
 export const GlobalConextProvider = ({children}) => {
     //informações globais
     let usuarioLogado = 'Melon musk'
+    const [usuarios, setUsuarios] = useState([
+        {
+            id: Date.now(),
+            nome: "ET Bilu",
+            email: "busquem@conhecimento.com.br"
+        },
+        {
+            id: Date.now()+1,
+            nome: "Mano Juca",
+            email: "mj-vidaloka@email.com"
+        },
+        {
+            id: Date.now()+2,
+            nome: "Melon Musk",
+            email: "melonMusk@spacex.com.br"
+        }
+    ])
     //informações globais
     return (
-        <GlobalConext.Provider value={{usuarioLogado}}>
+        <GlobalConext.Provider value={{usuarioLogado, usuarios, setUsuarios}}>
             {children}
         </GlobalConext.Provider>
     )
--- projetoRouter/src/pages/Home.jsx
+++ projetoRouter/src/pages/Home.jsx
@@ -3,12 +3,35 @@ import Navbar from '../components/Navbar'
 import { GlobalConext } from '../contexts/GlobalConext'
 
 function Home() {
-  const { usuarioLogado } = useContext(GlobalConext)
+  const { usuarioLogado, usuarios, setUsuarios } = useContext(GlobalConext)
+
+  function adicionarUsuario() {
+    let usuarioNovo = {
+      id: Date.now(),
+      nome: "Capitão Patria",
+      email: "caps@email.com"
+    }
+    setUsuarios([...usuarios, usuarioNovo])
+  }
   return (
     <div>
       <Navbar />
       <h1>Home</h1>
       <p>Olá {usuarioLogado}</p>
+      <div>
+        {
+          usuarios.map((usuario) => (
+            <div key={usuario.id}>
+              <p>Nome: {usuario.nome}</p>
+              <p>Email: {usuario.email}</p>
+              <p>{usuario.id}</p>
+            </div>
+          ))
+        }
+      </div>
+      <div>
+        <button onClick={adicionarUsuario}>Adicionar usúario</button>
+      </div>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam illo perspiciatis non sunt voluptatibus minima atque eius numquam facilis quam reprehenderit reiciendis a commodi cum natus autem, illum velit pariatur.</p>
     </div>
   )
